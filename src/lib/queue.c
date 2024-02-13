37044cd6e24601290009a006930b0d8aeda13bea
Andrea Piccin <andrea.piccin02@gmail.com>
2024-02-13 19:32:39 +0100

    style: code formatting

--- src/lib/queue.c
+++ src/lib/queue.c
@@ -1,5 +1,5 @@
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 
 #include "../../inc/queue.h"
@@ -15,18 +15,18 @@ bool queue_isEmpty(const StringQueue *queue) { return queue->itemCount == 0; }
 bool queue_isFull(const StringQueue *queue) { return queue->itemCount == QUEUE_SIZE; }
 
 void queue_enqueue(StringQueue *queue, const char *string) {
-    if (!queue_isFull(queue) && strlen(string)<QUEUE_ELEMENT_SIZE) {
+    if (!queue_isFull(queue) && strlen(string) < QUEUE_ELEMENT_SIZE) {
         queue->rear = (queue->rear + 1) % QUEUE_SIZE;
         strcpy(queue->data[queue->rear], string);
         queue->itemCount++;
     }
 }
 
-char* queue_dequeue(StringQueue *queue) {
+char *queue_dequeue(StringQueue *queue) {
     if (queue_isEmpty(queue))
         return NULL;
 
-    char* string = queue->data[queue->front];
+    char *string = queue->data[queue->front];
     queue->front = (queue->front + 1) % QUEUE_SIZE;
     queue->itemCount--;
     if (queue->itemCount == 0) {
@@ -36,8 +36,8 @@ char* queue_dequeue(StringQueue *queue) {
     return string;
 }
 
-char* queue_front(StringQueue *queue) {
-    if(queue_isEmpty(queue))
+char *queue_front(StringQueue *queue) {
+    if (queue_isEmpty(queue))
         return NULL;
     return queue->data[queue->front];
 }
