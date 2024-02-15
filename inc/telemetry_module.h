c124a8a7efc1905baed8f119143e2d6ecd09ec53
Matteo Frizzera <frizz@HP-3xxx-5825u.homenet.telecomitalia.it>
2024-02-15 02:45:06 +0100

    renamed telemetry module, worked on battery telemetry

--- inc/telemetry_module.h
+++ inc/telemetry_module.h
@@ -1,5 +1,5 @@
 /*H************************************************************************************************
- * FILENAME:        bluetooth_module.h
+ * FILENAME:        telemetry_module.h
  *
  * DESCRIPTION:
  *      This header files contains the definitions of high level functions to send and receive messages
@@ -24,6 +24,9 @@
 #include <stdint.h>
 #include "../motor_hal.h"
 
+#ifndef TELEMETRY_MODULE_H
+#define TELEMETRY_MODULE_H
+
 /*T************************************************************************************************
  * NAME: eventSeverity
  *
@@ -36,7 +39,7 @@
  *              MEDIUM_SEVERITY     event of medium or average importance
  *              HIGH_SEVERITY       most important event, relating to crucial and key parts of the system
  */
-typedef enum { LOW_SEVERITY, MEDIUM_SEVERITY, HIGH_SEVERITY } eventSeverity;
+typedef enum { LOW_SEVERITY, MEDIUM_SEVERITY, HIGH_SEVERITY } EventSeverity;
 
 
 /*T************************************************************************************************
@@ -51,25 +54,44 @@ typedef enum { LOW_SEVERITY, MEDIUM_SEVERITY, HIGH_SEVERITY } eventSeverity;
  *              BATTERY_PERCENTAGE_UPDATE       updates battery status, for example when battery is decreasing
  *              SPEED_UPDATE                    speed of motor has changed
  */
-typedef enum { OBJECT_DETECTED, BATTERY_PERCENTAGE_UPDATE, SPEED_UPDATE } eventType;
+typedef enum { OBJECT_DETECTED, BATTERY_PERCENTAGE_UPDATE, SPEED_UPDATE } EventType;
 
 
-struct MessageObjectDetected {
+typedef struct Message_InfoStruct {
+    EventSeverity severity;
+    EventType type;
+};
+typedef struct Message_InfoStruct Message_Info;
+
+
+typedef struct Message_ObjectDetectedStruct {
+    Message_Info messageInfo;
     uint8_t  direction;
     uint16_t distance;
 };
+typedef struct Message_ObjectDetectedStruct Message_ObjectDetected;
 
-struct MessageBatteryPercentageUpdate {
+
+typedef struct Message_BatteryPercentageUpdateStruct {
+    Message_Info messageInfo;
     uint8_t percentage;
 };
+typedef struct Message_BatteryPercentageUpdateStruct Message_BatteryPercentageUpdate;
 
-struct MessageMotorSpeedUpdate {
+
+typedef struct Message_MotorSpeedUpdateStruct {
+    Message_Info messageInfo;
     uint8_t speed;
 };
+typedef struct Message_MotorSpeedUpdateStruct Message_MotorSpeedUpdate;
 
-struct MessageMotorDirectionUpdate {
+
+typedef struct Message_MotorDirectionUpdateStruct {
+    Message_Info messageInfo;
     MotorDirection direction;
 };
+typedef struct Message_MotorDirectionUpdateStruct Message_MotorDirectionUpdate;
 
 
+#endif // TELEMETRY_MODULE_H
 
