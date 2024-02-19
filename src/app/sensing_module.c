ad79713e36b1525ae396a4f4d149ebba7a61bf42
Andrea Piccin <andrea.piccin02@gmail.com>
2024-02-19 17:54:33 +0100

    feat: single and double measurements callback

--- src/app/sensing_module.c
+++ src/app/sensing_module.c
@@ -7,11 +7,12 @@
  *      left, or on the right of the msp432car.
  *
  * PUBLIC FUNCTIONS:
- *      void        Sensing_Module_init()
- *      void        Sensing_Module_checkClearance(uint8_t deg)
- *      void        Sensing_Module_checkLeftClearance()
- *      void        Sensing_Module_checkRightClearance()
- *      void        Sensing_Module_checkFrontClearance()
+ *      void    Sensing_Module_init()
+ *      void    Sensing_Module_checkClearance(uint8_t deg)
+ *      void    Sensing_Module_checkLateralClearance()
+ *      void    Sensing_Module_checkFrontClearance()
+ *      void    Sensing_Module_registerSingleMeasurementReadyCallback(SensingSingleCallback call)
+ *      void    Sensing_Module_registerDoubleMeasurementReadyCallback(SensingDoubleCallback call)
  *
  * NOTES:
  *
@@ -23,8 +24,11 @@
  * DATE         AUTHOR              DETAIL
  * 15 Feb 2024  Matteo Frizzera     Added polling to wait for servo to reach target direction
  * 16 Feb 2024  Andrea Piccin       Refactoring, removed busy waiting mechanism
- * 19 Feb 2024  Andrea Piccin       Ultrasonic measurement callback moved to state_machine.c
+ * 19 Feb 2024  Andrea Piccin       Single (front) and Double (lateral) measurements callbacks
  */
+#include <stdbool.h>
+#include <stddef.h>
+
 #include "../../inc/sensing_module.h"
 #include "../../inc/servo_hal.h"
 #include "../../inc/ultrasonic_hal.h"
@@ -32,9 +36,34 @@
 #define SERVO_POS_LEFT SERVO_MAX_POSITION  /* Position left                                     */
 #define SERVO_POS_FRONT 0                  /* Position front, meaning in between right and left */
 #define SERVO_POS_RIGHT SERVO_MIN_POSITION /* Position right                                    */
-#define SENSING_MIN_DISTANCE_THRESHOLD 15  /* Minimum allowed distance before stopping the car  */
+#define SENSING_FREE_THRESHOLD 20          /* Minimum allowed clearance                         */
 
-Servo servo; /* servo motor on which the ultrasonic sensor is mounted */
+/* Utility function declaration */
+void Sensing_Module_onUSMeasurementReady(uint16_t distance);
+
+/*T************************************************************************************************
+ * NAME: SensingMode
+ *
+ * DESCRIPTION:
+ *      Specifies if the next measurement is single (e.g. front) or double (e.g. lateral)
+ *
+ * SPECIFICATIONS:
+ *      Type:   enum
+ *      Values: SENSING_SINGLE_SAMPLE_MODE
+ *              SENSING_DOUBLE_SAMPLE_MODE
+ */
+typedef enum {
+    SENSING_SINGLE_SAMPLE_MODE,
+    SENSING_DOUBLE_SAMPLE_MODE,
+} SensingMode;
+
+Servo servo;                           /* Servo motor on which the ultrasonic sensor is mounted  */
+SensingMode currentSensingMode;        /* Represent the current operating mode                   */
+SensingSingleCallback singleCallback;  /* Callback function for single measurement               */
+SensingDoubleCallback doubleCallback; /* Callback function for double measurements              */
+volatile uint16_t previousSample;      /* Value of the last measurement (for double samples)     */
+volatile int8_t nextDirection;         /* Direction of the next measurement (for double samples) */
+volatile uint8_t sampleCount;          /* Count of the taken samples (for double samples)        */
 
 /*F************************************************************************************************
  * NAME: void Sensing_Module_init()
@@ -61,10 +90,15 @@ void Sensing_Module_init() {
     US_HAL_init();
     SERVO_HAL_init(&servo);
     SERVO_HAL_registerPositionReachedCallback(US_HAL_triggerMeasurement);
+    US_HAL_registerMeasurementCallback(Sensing_Module_onUSMeasurementReady);
+    currentSensingMode = SENSING_SINGLE_SAMPLE_MODE;
+    singleCallback = NULL;
+    doubleCallback = NULL;
+    sampleCount = 0;
 }
 
 /*F************************************************************************************************
- * NAME: void Sensing_Module_checkClearance(uint8_t deg)
+ * NAME: void Sensing_Module_checkSingleClearance(int8_t deg)
  *
  * DESCRIPTION:
  *      Moves motor to specified direction (from left -90 deg to right 90 deg) then uses ultrasonic
@@ -86,14 +120,47 @@ void Sensing_Module_init() {
  *
  *  NOTE:
  */
-void Sensing_Module_checkClearance(uint8_t deg) { SERVO_HAL_setPosition(&servo, deg); }
+void Sensing_Module_checkSingleClearance(int8_t deg) {
+    currentSensingMode = SENSING_SINGLE_SAMPLE_MODE;
+    SERVO_HAL_setPosition(&servo, deg);
+}
 
 /*F************************************************************************************************
- * NAME: void Sensing_Module_checkLeftClearance
+ * NAME: void Sensing_Module_checkDoubleClearance(uint8_t deg)
  *
  * DESCRIPTION:
- *      Calls general function Sensing_Module_checkClearance to check wether there is an object on
- *      the left.
+ *      Moves motor to the specified direction (from left -90 deg to right 90 deg) then uses
+ *      ultrasonic sensor to check if there is an object in that direction, repeats the measurement
+ *      on the second target direction.
+ *      When the servo reaches its final position it will automatically trigger a new ultrasonic
+ *      measurement.
+ *
+ * INPUTS:
+ *      PARAMETERS:
+ *          uint8_t deg     first direction to check
+ *          uint8_t deg     second direction to check
+ *      GLOBALS:
+ *          Servo   servo   servo motor to rotate
+ *
+ *  OUTPUTS:
+ *      PARAMETERS:
+ *          None
+ *      GLOBALS:
+ *          None
+ *
+ *  NOTE:
+ */
+void Sensing_Module_checkDoubleClearance(int8_t deg1, int8_t deg2) {
+    currentSensingMode = SENSING_DOUBLE_SAMPLE_MODE;
+    nextDirection = deg2;
+    SERVO_HAL_setPosition(&servo, deg1);
+}
+
+/*F************************************************************************************************
+ * NAME: void Sensing_Module_checkLateralClearance
+ *
+ * DESCRIPTION:
+ *      Checks if there is an object on the left or on the right of the robot.
  *
  * INPUTS:
  *      PARAMETERS:
@@ -109,33 +176,12 @@ void Sensing_Module_checkClearance(uint8_t deg) { SERVO_HAL_setPosition(&servo, 
  *
  *  NOTE:
  */
-void Sensing_Module_checkLeftClearance() { Sensing_Module_checkClearance(SERVO_POS_LEFT); }
+void Sensing_Module_checkLateralClearance() {
+    Sensing_Module_checkDoubleClearance(SERVO_POS_LEFT, SERVO_POS_RIGHT);
+}
 
 /*F************************************************************************************************
- * NAME: void Sensing_Module_checkRightClearance
- *
- * DESCRIPTION:
- *      Calls general function Sensing_Module_checkClearance to check wether there is an object on
- *      the right.
- *
- * INPUTS:
- *      PARAMETERS:
- *          None
- *      GLOBALS:
- *          Servo servo                                      servo motor to rotate
- *
- *  OUTPUTS:
- *      PARAMETERS:
- *          None
- *      GLOBALS:
-            None
- *
- *  NOTE:
- */
-void Sensing_Module_checkRightClearance() { Sensing_Module_checkClearance(SERVO_POS_RIGHT); }
-
-/*F************************************************************************************************
- * NAME: void Sensing_Module_checkClearance_FRONT
+ * NAME: void Sensing_Module_checkFrontClearance()
  *
  * DESCRIPTION:
  *      Calls general function Sensing_Module_checkClearance to check wether there is an object in
@@ -155,4 +201,99 @@ void Sensing_Module_checkRightClearance() { Sensing_Module_checkClearance(SERVO_
  *
  *  NOTE:
  */
-void Sensing_Module_checkFrontClearance() { Sensing_Module_checkClearance(SERVO_POS_FRONT); }
+void Sensing_Module_checkFrontClearance() { Sensing_Module_checkSingleClearance(SERVO_POS_FRONT); }
+
+/*F************************************************************************************************
+ * NAME: void Sensing_Module_registerSingleMeasurementReadyCallback(SensingSingleCallback callback)
+ *
+ * DESCRIPTION:
+ *      Registers the SensingSingleCallback as the function to call when a single measurement
+ *      is ready.
+ *
+ * INPUTS:
+ *      PARAMETERS:
+ *          SensingSingleCallback    callback           The function to register as callback
+ *      GLOBALS:
+ *          None
+ *
+ *  OUTPUTS:
+ *      PARAMETERS:
+ *          None
+ *      GLOBALS:
+ *          SensingSingleCallback    singleCallback      Set to the given function
+ *
+ *  NOTE:
+ */
+void Sensing_Module_registerSingleMeasurementReadyCallback(SensingSingleCallback callback) {
+    singleCallback = callback;
+}
+
+/*F************************************************************************************************
+ * NAME: Sensing_Module_registerDoubleMeasurementReadyCallback(SensingDoubleCallback callback)
+ *
+ * DESCRIPTION:
+ *      Registers the SensingDoubleCallback as the function to call when the double measurement
+ *      is ready.
+ *
+ * INPUTS:
+ *      PARAMETERS:
+ *          SensingDoubleCallback  callback           The function to register as callback
+ *      GLOBALS:
+ *          None
+ *
+ *  OUTPUTS:
+ *      PARAMETERS:
+ *          None
+ *      GLOBALS:
+ *          SensingDoubleCallback  doubleCallback     Set to the given function
+ *
+ *  NOTE:
+ */
+void Sensing_Module_registerDoubleMeasurementReadyCallback(SensingDoubleCallback callback) {
+    doubleCallback = callback;
+}
+
+/*F************************************************************************************************
+ * NAME: void Sensing_Module_onUSMeasurementReady(uint16_t distance)
+ *
+ * DESCRIPTION:
+ *      This function acts as callback function when a measurement of the ultrasonic sensor ends,
+ *      there are two possible function modes:
+ *      - SINGLE_SAMPLE_MODE: the request is for a single measurement (e.g. for front measurements)
+ *                            so when the result is ready we'll forward it to the callback function
+ *       - DOUBLE_SAMPLE_MODE: the request is for a double measurement (e.g. left & right) so
+ *                            when the first measurement is ready we'll store it and when also
+ *                            the second is ready we'll call the double callback function.
+ *
+ * INPUTS:
+ *      PARAMETERS:
+ *          None
+ *      GLOBALS:
+ *          None
+ *
+ *  OUTPUTS:
+ *      PARAMETERS:
+ *          None
+ *      GLOBALS:
+ *          None
+ *
+ *  NOTE:
+ */
+void Sensing_Module_onUSMeasurementReady(uint16_t distance) {
+    if (currentSensingMode == SENSING_SINGLE_SAMPLE_MODE) {
+        if (singleCallback != NULL)
+            singleCallback(distance > SENSING_FREE_THRESHOLD);
+    } else { // double sensing mode
+        sampleCount++;
+        if (sampleCount < 2) {
+            previousSample = distance;
+            SERVO_HAL_setPosition(&servo, nextDirection);
+        } else {
+            sampleCount = 0;
+            bool isDir1Free = previousSample > SENSING_FREE_THRESHOLD;
+            bool isDir2Free = distance > SENSING_FREE_THRESHOLD;
+            if (doubleCallback != NULL)
+                doubleCallback(isDir1Free, isDir2Free);
+        }
+    }
+}
