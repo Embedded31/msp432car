122bec8bd65b78ff1e5f2507d0170f771c13c509
Andrea Piccin <andrea.piccin02@gmail.com>
2024-02-19 16:07:56 +0100

    feat: remote control module

---
+++ src/app/remote_module.c
@@ -0,0 +1,109 @@
+/*H************************************************************************************************
+ * FILENAME:        remote_module.c
+ *
+ * DESCRIPTION:
+ *      This source file contains the implementations of high level functions to manually control
+ *      the motors using IR commands.
+ *
+ * PUBLIC FUNCTIONS:
+ *      void        Remote_Module_init()
+ *      void        Remote_Module_registerAutoModeRequestCallback(RemoteCallback callback);
+ *
+ * NOTES:
+ *
+ * AUTHOR: Andrea Piccin    <andrea.piccin@studenti.unitn.it>
+ *
+ * START DATE: 19 Feb 2024
+ *
+ * CHANGES:
+ */
+#include <stdbool.h>
+#include <string.h>
+
+#include "../../inc/remote_module.h"
+#include "../../inc/powertrain_module.h"
+#include "../../inc/state_machine.h"
+#include "../../inc/infrared_hal.h"
+#include "../../inc/bluetooth_hal.h"
+
+RemoteCallback remoteCallback;
+
+void Remote_Module_onIRMessageReceived(IRCommand command, bool isValid){
+    if(FSM_currentState != STATE_REMOTE && command != IR_COMMAND_ASTERISK)
+        return;
+
+    if(isValid){
+        switch (command) {
+            case IR_COMMAND_UP:         /* Start motors forward at default speed  */
+                Powertrain_Module_moveForward();
+                break;
+            case IR_COMMAND_DOWN:       /* Start motors backward ad default speed */
+                Powertrain_Module_moveBackward();
+                break;
+            case IR_COMMAND_LEFT:       /* Rotate 45 deg CCW                      */
+                Powertrain_Module_turnLeft(45);
+                break;
+            case IR_COMMAND_RIGHT:      /* Rotate 45 deg CW                       */
+                Powertrain_Module_turnRight(45);
+                break;
+            case IR_COMMAND_OK:         /* Stop the motors                        */
+                Powertrain_Module_stop();
+                break;
+            case IR_COMMAND_2:          /* Increase speed                         */
+                Powertrain_Module_increaseSpeed();
+                break;
+            case IR_COMMAND_8:          /* Decrease speed                         */
+                Powertrain_Module_decreaseSpeed();
+                break;
+            case IR_COMMAND_ASTERISK:   /* Switch to autonomous mode              */
+                if(remoteCallback!=NULL)
+                    remoteCallback();
+                break;
+            case IR_COMMAND_HASHTAG:    /* Take an ultrasonic sample              */
+                if(remoteCallback!=NULL)
+                    remoteCallback();
+                break;
+            default:                    /* Nothing to do                          */
+                break;
+        }
+    }
+}
+
+void Remote_Module_onBTMessageReceived(const char* message){
+    char command[4];
+    command[3] = '\0';
+    strncpy(command, message, 3);
+
+    if(FSM_currentState != STATE_REMOTE && strcmp(command, "MAN")!=0)
+        return;
+
+    if(strcmp(command, "FWD")==0){          /* Start motors forward at default speed  */
+        Powertrain_Module_moveForward();
+    } else if(strcmp(command, "REV")==0){   /* Start motors backward at default speed */
+        Powertrain_Module_moveBackward();
+    } else if(strcmp(command, "LFT")==0){   /* Rotate 45 deg CCW                      */
+        Powertrain_Module_turnLeft(45);
+    } else if(strcmp(command, "RGT")==0){   /* Rotate 45 deg CW                       */
+        Powertrain_Module_turnRight(45);
+    } else if(strcmp(command, "STP")==0){   /* Stop the motors                        */
+        Powertrain_Module_stop();
+    } else if(strcmp(command, "AUT")==0){   /* Switch to autonomous mode              */
+        if(remoteCallback!=NULL)
+            remoteCallback();
+    } else if(strcmp(command, "MAN")==0){   /* Switch to manual mode                  */
+        if(remoteCallback!=NULL)
+            remoteCallback();
+    }
+}
+
+void Remote_Module_init(){
+    IR_HAL_init();
+    IR_HAL_registerMessageCallback(Remote_Module_onIRMessageReceived);
+    BT_HAL_registerMessageCallback(Remote_Module_onBTMessageReceived);
+    remoteCallback = NULL;
+}
+
+void Remote_Module_registerModeChangeRequestCallback(RemoteCallback callback){
+    remoteCallback = callback;
+}
+
